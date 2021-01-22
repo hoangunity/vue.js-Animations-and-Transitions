8620069e9c694cbb0c81fbe1a2464418450291d8
hoangunity <hoangdm.141196@gmail.com>
2021-01-23 01:51:26 +0700

    CSS Animations with the Transition Component

--- src/App.vue
+++ src/App.vue
@@ -91,35 +91,36 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.v-enter-from {
+/* .v-enter-from {
   opacity: 0;
   transform: translateY(-30px);
-}
+} */
 
 .v-enter-active {
-  transition: all 0.3s ease-out;
+  animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-to {
+/* .v-enter-to {
   opacity: 1;
   transform: translateY(0);
-}
+} */
 
-.v-leave-from {
+/* .v-leave-from {
   opacity: 1;
   transform: translateY(0);
-}
+} */
 
 .v-leave-active {
-  transition: all 0.3s ease-in;
+  /* transition: all 0.3s ease-in; */
+  animation: slide-scale 0.3s ease-out;
 }
 
-.v-leave-to {
+/* .v-leave-to {
   opacity: 0;
   transform: translateX(50px);
-}
+} */
 
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
